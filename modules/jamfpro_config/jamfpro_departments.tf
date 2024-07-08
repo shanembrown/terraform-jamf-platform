1b94f6b68556b8b65c15919d8c1f21a6867daa42
vbnin-jamf <vincent.bonnin@jamf.com>
2024-07-08 14:32:34 +0200

    Added buildings, departements and sites

---
+++ modules/jamfpro_config/jamfpro_departments.tf
@@ -0,0 +1,15 @@
+resource "jamfpro_department" "department_executives" {
+  name = "Executives"
+}
+
+resource "jamfpro_department" "department_sales" {
+  name = "Sales"
+}
+
+resource "jamfpro_department" "department_support" {
+  name = "Support"
+}
+
+resource "jamfpro_department" "department_engineering" {
+  name = "Engineering"
+}
