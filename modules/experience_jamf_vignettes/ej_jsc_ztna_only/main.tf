a536c82740fd44eac53c31e2a86f5cf66e88cde6
leggatron <ryan.legg@jamf.com>
2024-07-25 13:34:08 -0400

    test

--- modules/experience_jamf_vignettes/ej_jsc_ztna_only/main.tf
+++ modules/experience_jamf_vignettes/ej_jsc_ztna_only/main.tf
@@ -1,10 +1,6 @@
 ## Call Terraform provider
 terraform {
   required_providers {
-    jamfpro = {
-      source  = "deploymenttheory/jamfpro"
-      version = "~> 0.1.5"
-    }
     jsc = {
       source = "danjamf/jsctfprovider"
       version = "0.0.5"
