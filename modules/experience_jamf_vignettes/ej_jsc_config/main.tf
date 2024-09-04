b847d23769b62e5089dc56b114131db2a33aeaed
leggatron <ryan.legg@jamf.com>
2024-09-04 13:55:31 -0400

    redeploy_on_update and sleep adjustments for UEM Connect

--- modules/experience_jamf_vignettes/ej_jsc_config/main.tf
+++ modules/experience_jamf_vignettes/ej_jsc_config/main.tf
@@ -51,6 +51,12 @@ output "jp_client_secret" {
   value = data.jamfpro_api_integration.jamf_pro_api_integration_001_data.client_secret
 }
 
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [jamfpro_api_integration.jamfpro_api_integration_jsc]
+
+  create_duration = "60s"
+}
+
 resource "jsc_uemc" "initial_uemc" {
   domain       = var.jamfpro_instance_url
   clientid     = local.jamf_pro_client_id
