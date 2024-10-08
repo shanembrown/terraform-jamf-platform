370bdc001102184f633a50ba778c979016535923
matthew.ward <matthew.ward@jamf.com>
2024-10-08 18:06:50 -0500

    fmt update

--- modules/onboarder_modules/jamf_pro_trial_kickstart/main.tf
+++ modules/onboarder_modules/jamf_pro_trial_kickstart/main.tf
@@ -20,85 +20,85 @@ resource "jamfpro_building" "building_headquarters" {
 }
 
 resource "jamfpro_building" "building_tokyo" {
-  name    = "Tokyo Office"
-  city    = "Tokyo"
-  country = "Japan"
-  depends_on = [ jamfpro_building.building_headquarters ]
+  name       = "Tokyo Office"
+  city       = "Tokyo"
+  country    = "Japan"
+  depends_on = [jamfpro_building.building_headquarters]
 }
 
 resource "jamfpro_building" "building_london" {
-  name    = "London Office"
-  city    = "London"
-  country = "UK"
-  depends_on = [ jamfpro_building.building_tokyo ]
+  name       = "London Office"
+  city       = "London"
+  country    = "UK"
+  depends_on = [jamfpro_building.building_tokyo]
 }
 
 resource "jamfpro_building" "building_paris" {
-  name    = "Paris Office"
-  city    = "Paris"
-  country = "France"
-  depends_on = [ jamfpro_building.building_london ]
+  name       = "Paris Office"
+  city       = "Paris"
+  country    = "France"
+  depends_on = [jamfpro_building.building_london]
 }
 
 
 #Categories
 
 resource "jamfpro_category" "category_developer_tools" {
-  name     = "Developer Tools"
-  priority = 9
-  depends_on = [ jamfpro_building.building_paris ]
+  name       = "Developer Tools"
+  priority   = 9
+  depends_on = [jamfpro_building.building_paris]
 }
 
 resource "jamfpro_category" "category_network" {
-  name     = "Network Security"
-  priority = 9
-  depends_on = [ jamfpro_category.category_developer_tools ]
+  name       = "Network Security"
+  priority   = 9
+  depends_on = [jamfpro_category.category_developer_tools]
 }
 
 resource "jamfpro_category" "category_printers" {
-  name     = "Printers"
-  priority = 9
-  depends_on = [ jamfpro_category.category_network ]
+  name       = "Printers"
+  priority   = 9
+  depends_on = [jamfpro_category.category_network]
 }
 
 resource "jamfpro_category" "category_productivity" {
-  name     = "Productivity"
-  priority = 9
-  depends_on = [ jamfpro_category.category_printers ]
+  name       = "Productivity"
+  priority   = 9
+  depends_on = [jamfpro_category.category_printers]
 }
 
 resource "jamfpro_category" "category_security_compliance" {
-  name     = "Security and Compliance"
-  priority = 9
-  depends_on = [ jamfpro_category.category_productivity ]
+  name       = "Security and Compliance"
+  priority   = 9
+  depends_on = [jamfpro_category.category_productivity]
 }
 
 resource "jamfpro_category" "category_uninstallers" {
-  name     = "Uninstallers"
-  priority = 9
-  depends_on = [ jamfpro_category.category_security_compliance ]
+  name       = "Uninstallers"
+  priority   = 9
+  depends_on = [jamfpro_category.category_security_compliance]
 }
 
 #Departments
 
 resource "jamfpro_department" "department_executives" {
-  name = "Executives"
-  depends_on = [ jamfpro_category.category_uninstallers ]
+  name       = "Executives"
+  depends_on = [jamfpro_category.category_uninstallers]
 }
 
 resource "jamfpro_department" "department_sales" {
-  name = "Sales"
-  depends_on = [ jamfpro_department.department_executives ]
+  name       = "Sales"
+  depends_on = [jamfpro_department.department_executives]
 }
 
 resource "jamfpro_department" "department_support" {
-  name = "Support"
-  depends_on = [ jamfpro_department.department_sales ]
+  name       = "Support"
+  depends_on = [jamfpro_department.department_sales]
 }
 
 resource "jamfpro_department" "department_engineering" {
-  name = "Engineering"
-  depends_on = [ jamfpro_department.department_support ]
+  name       = "Engineering"
+  depends_on = [jamfpro_department.department_support]
 }
 
 # Smart Groups
@@ -116,5 +116,5 @@ resource "jamfpro_smart_computer_group" "group_OS_version" {
     value       = "15"
     priority    = 0
   }
-  depends_on = [ jamfpro_department.department_engineering ]
+  depends_on = [jamfpro_department.department_engineering]
 }
