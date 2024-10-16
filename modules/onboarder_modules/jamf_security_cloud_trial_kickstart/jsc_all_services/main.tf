78935a6b31cdf874f40f9c135c3c157aa4e37b5e
leggatron <ryan.legg@jamf.com>
2024-10-16 12:35:34 -0400

    initial updates

--- modules/onboarder_modules/jamf_security_cloud_trial_kickstart/jsc_all_services/main.tf
+++ modules/onboarder_modules/jamf_security_cloud_trial_kickstart/jsc_all_services/main.tf
@@ -27,7 +27,7 @@ resource "jsc_ap" "all_services" {
   datapolicy       = true
 }
 
-resource "jamfpro_macos_configuration_profile_plist" "dp" {
+resource "jamfpro_macos_configuration_profile_plist" "all_services_macos" {
   name                = "Jamf Connect ZTNA + Jamf Protect Threat and Content Control - macOS (Supervised)"
   distribution_method = "Install Automatically"
   redeploy_on_update  = "Newly Assigned"
@@ -40,6 +40,54 @@ resource "jamfpro_macos_configuration_profile_plist" "dp" {
     all_computers = false
   }
   depends_on = [jsc_ap.all_services]
+}
+
+resource "jamfpro_smart_mobile_device_group" "supervised_ios" {
+  name = "Jamf Security Cloud - Supervised Devices"
+
+  criteria {
+    name = "Supervised"
+    priority = 0
+    search_type = "is"
+    value = "Supervised"
+  }
+}
+
+resource "jamfpro_mobile_device_configuration_profile_plist" "all_services_ios_supervised" {
+  name = "Jamf Connect ZTNA + Jamf Protect Threat and Content Control - iOS / iPadOS (Supervised)"
+  redeploy_on_update = "Newly Assigned"
+  
+  payloads = jsc_ap.all_services.supervisedplist
+  payload_validate = false
+
+  scope {
+    all_mobile_devices = false
+  }
+  depends_on = [ jamfpro_macos_configuration_profile_plist.all_services_macos ]
+}
+
+resource "jamfpro_smart_mobile_device_group" "unsupervised_ios" {
+  name = "Jamf Security Cloud - Un-Supervised Devices"
+
+  criteria {
+    name = "Supervised"
+    priority = 0
+    search_type = "is"
+    value = "Unsupervised"
+  }
+}
+
+resource "jamfpro_mobile_device_configuration_profile_plist" "all_services_ios_unsupervised" {
+  name = "Jamf Connect ZTNA + Jamf Protect Threat and Content Control - iOS / iPadOS (Un-Supervised)"
+  redeploy_on_update = "Newly Assigned"
+  
+  payloads = jsc_ap.all_services.unsupervisedplist
+  payload_validate = false
+
+  scope {
+    all_mobile_devices = false
+  }
+  depends_on = [ jamfpro_mobile_device_configuration_profile_plist.all_services_ios_supervised ]
 }
 
 output "enable_jsc_uemc_output" {
