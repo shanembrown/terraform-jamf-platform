fdd315aad2c01680b19a1caad83eadaee975a012
leggatron <ryan.legg@jamf.com>
2025-01-27 11:28:10 -0500

    removed ej modules and module calls in main.tf

--- CLEANUP_BEFORE_PUBLIC_REPO/experience_jamf_vignettes/ej_mac_cis_benchmark/support_files/computer_extension_attributes/sonoma_cis_apply.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -e /Library/.tje/apply_cis ]; then
-    echo "<result>apply_cis</result>"
-else
-    echo "<result></result>"
-fi
-
-exit 0
--- CLEANUP_BEFORE_PUBLIC_REPO/experience_jamf_vignettes/ej_mac_cis_benchmark/support_files/computer_extension_attributes/sonoma_cis_remove.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -e /Library/.tje/remove_cis ]; then
-    echo "<result>remove_cis</result>"
-else
-    echo "<result></result>"
-fi
-
-exit 0
