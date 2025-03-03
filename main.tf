38a42f6d554901109990915462dbd96944a9a4a6
danjamf <128504575+danjamf@users.noreply.github.com>
2025-03-03 08:51:35 -0800

    Revert "Target Module Update (#335)" (#336)
    This reverts commit ffc1bf161a12c82e5eb26dd90b317958a04ff890.

--- main.tf
+++ main.tf
@@ -37,26 +37,32 @@ provider "jsc" {
 
 # Onboarder Modules
 module "onboarder-all" {
+  count                     = var.include_onboarder == true ? 1 : 0
   source                    = "./modules/onboarder-all"
   support_files_path_prefix = var.support_files_path_prefix
 }
 
 module "onboarder-management-macOS" {
+  count                     = var.include_onboarder == true ? 1 : 0
   source                    = "./modules/onboarder-management-macOS"
   support_files_path_prefix = var.support_files_path_prefix
 }
 
 module "onboarder-management-mobile" {
+  count                     = var.include_onboarder == true ? 1 : 0
   source                    = "./modules/onboarder-management-mobile"
   support_files_path_prefix = var.support_files_path_prefix
 }
 
 module "onboarder-security" {
+
+  count                     = var.include_onboarder == true ? 1 : 0
   source                    = "./modules/onboarder-security"
   support_files_path_prefix = var.support_files_path_prefix
 }
 
 module "onboarder-app-installers" {
+  count                     = var.include_onboarder == true ? 1 : 0
   source                    = "./modules/onboarder-app-installers"
   support_files_path_prefix = var.support_files_path_prefix
 }
@@ -65,9 +71,8 @@ module "onboarder-app-installers" {
 
 ## Initialize Protect (for macOS) module
 
-
-module "configuration-jamf-pro-jamf-protect" {
-
+module "jamf_protect_trial_kickstart" {
+  count                       = var.include_jamf_protect_trial_kickstart == true ? 1 : 0
   source                      = "./modules/configuration-jamf-pro-jamf-protect"
   jamfpro_instance_url        = var.jamfpro_instance_url
   jamfpro_client_id           = var.jamfpro_client_id
@@ -77,147 +82,169 @@ module "configuration-jamf-pro-jamf-protect" {
   jamfprotect_client_password = var.jamfprotect_client_password
 }
 
-
-module "compliance-macOS-cis-level-1" {
+module "mac_cis_lvl1_benchmark" {
+  count                     = var.include_mac_cis_lvl1_benchmark == true ? 1 : 0
   source                    = "./modules/compliance-macOS-cis-level-1"
   support_files_path_prefix = var.support_files_path_prefix
 }
 
-
-module "compliance-iOS-cis-level-1" {
+module "mobile_cis_lvl1_benchmark" {
+  count                     = var.include_mobile_cis_lvl1_benchmark == true ? 1 : 0
   source                    = "./modules/compliance-iOS-cis-level-1"
   support_files_path_prefix = var.support_files_path_prefix
 }
 
-
-module "compliance-macOS-disa-stig" {
+module "mac_stig_benchmark" {
+  count                     = var.include_mac_stig_benchmark == true ? 1 : 0
   source                    = "./modules/compliance-macOS-disa-stig"
   support_files_path_prefix = var.support_files_path_prefix
 }
 
-
-module "compliance-iOS-disa-stig" {
+module "mobile_stig_benchmark" {
+  count                     = var.include_mobile_stig_benchmark == true ? 1 : 0
   source                    = "./modules/compliance-iOS-disa-stig"
   support_files_path_prefix = var.support_files_path_prefix
 }
 
-
-module "compliance-macOS-nist-800-171" {
+module "mac_800_171_benchmark" {
+  count                     = var.include_mac_800_171_benchmark == true ? 1 : 0
   source                    = "./modules/compliance-macOS-nist-800-171"
   support_files_path_prefix = var.support_files_path_prefix
 }
 
-module "compliance-macOS-cmmc-level-1" {
+module "mac_cmmc_lvl1_benchmark" {
+  count                     = var.include_mac_cmmc_lvl1_benchmark == true ? 1 : 0
   source                    = "./modules/compliance-macOS-cmmc-level-1"
   support_files_path_prefix = var.support_files_path_prefix
 }
 
-
-module "configuration-jamf-pro-smart-groups" {
+module "qol_smart_groups" {
+  count  = var.include_qol_smart_groups == true ? 1 : 0
   source = "./modules/configuration-jamf-pro-smart-groups"
 }
 
-module "management-app-installers-microsoft-365" {
+module "microsoft_365" {
+  count  = var.include_microsoft_365 == true ? 1 : 0
   source = "./modules/management-app-installers-microsoft-365"
 }
 
-module "configuration-jamf-pro-categories" {
+module "categories" {
+  count  = var.include_categories == true ? 1 : 0
   source = "./modules/configuration-jamf-pro-categories"
 }
 
-module "management-iOS-configuration-profiles" {
+module "mobile_device_kickstart" {
+  count  = var.include_mobile_device_kickstart == true ? 1 : 0
   source = "./modules/management-iOS-configuration-profiles"
 }
 
-module "configuration-jamf-pro-computer-management-settings" {
+module "computer_management_settings" {
+  count  = var.include_computer_management_settings == true ? 1 : 0
   source = "./modules/configuration-jamf-pro-computer-management-settings"
 }
 
-module "endpoint-security-macOS-filevault" {
+module "filevault" {
+  count  = var.include_filevault == true ? 1 : 0
   source = "./modules/endpoint-security-macOS-filevault"
 }
 
-module "endpoint-security-macOS-microsoft-defender" {
+module "msft_defender" {
+  count  = var.include_defender == true ? 1 : 0
   source = "./modules/endpoint-security-macOS-microsoft-defender"
 }
 
-module "management-macOS-passwordless-sso" {
+module "passwordless_sso" {
+  count                     = var.include_passwordless_ssoe == true ? 1 : 0
   source                    = "./modules/management-macOS-passwordless-sso"
   support_files_path_prefix = var.support_files_path_prefix
 }
 
-
-module "endpoint-security-macOS-crowdstrike" {
+module "crowdstrike" {
+  count  = var.include_crowdstrike == true ? 1 : 0
   source = "./modules/endpoint-security-macOS-crowdstrike"
 }
 
 
-module "management-macOS-rosetta" {
+module "rosetta" {
+  count  = var.include_rosetta == true ? 1 : 0
   source = "./modules/management-macOS-rosetta"
 }
 
-module "management-app-installers-box-drive" {
+module "box_drive" {
+  count  = var.include_box_drive == true ? 1 : 0
   source = "./modules/management-app-installers-box-drive"
 }
 
-module "management-app-installers-nudge" {
+module "nudge" {
+  count  = var.include_nudge == true ? 1 : 0
   source = "./modules/management-app-installers-nudge"
 }
 
-module "management-app-installers-adobe-creative-cloud" {
+module "adobe_creative_cloud" {
+  count  = var.include_adobe_creative_cloud == true ? 1 : 0
   source = "./modules/management-app-installers-adobe-creative-cloud"
 }
 
-module "management-app-installers-text-expander" {
+module "text_expander" {
+  count  = var.include_text_expander == true ? 1 : 0
   source = "./modules/management-app-installers-text-expander"
 }
 
-
-module "management-app-installers-microsoft-edge" {
+module "microsoft_edge" {
+  count  = var.include_microsoft_edge == true ? 1 : 0
   source = "./modules/management-app-installers-microsoft-edge"
 }
 
-module "management-app-installers-google-chrome" {
+module "google_chrome" {
+  count  = var.include_google_chrome == true ? 1 : 0
   source = "./modules/management-app-installers-google-chrome"
 }
 
-module "management-app-installers-mozilla-firefox" {
+module "mozilla_firefox" {
+  count  = var.include_mozilla_firefox == true ? 1 : 0
   source = "./modules/management-app-installers-mozilla-firefox"
 }
 
-module "management-app-installers-slack" {
+module "slack" {
+  count  = var.include_slack == true ? 1 : 0
   source = "./modules/management-app-installers-slack"
 }
 
-module "management-app-installers-dropbox" {
+module "dropbox" {
+  count  = var.include_dropbox == true ? 1 : 0
   source = "./modules/management-app-installers-dropbox"
 }
 
-module "management-app-installers-google-drive" {
+module "google_drive" {
+  count  = var.include_google_drive == true ? 1 : 0
   source = "./modules/management-app-installers-google-drive"
 }
 
-module "management-app-installers-jamf-composer" {
+module "jamf_composer" {
+  count  = var.include_jamf_composer == true ? 1 : 0
   source = "./modules/management-app-installers-jamf-composer"
 }
 
-module "management-app-installers-pppc-utility" {
+module "pppc_utility" {
+  count  = var.include_pppc_utility == true ? 1 : 0
   source = "./modules/management-app-installers-pppc-utility"
 }
 
-module "management-app-installers-jamfcheck" {
+module "jamfcheck" {
+  count  = var.include_jamfcheck == true ? 1 : 0
   source = "./modules/management-app-installers-jamfcheck"
 }
 
-module "management-app-installers-zoom" {
+module "zoom" {
+  count  = var.include_zoom == true ? 1 : 0
   source = "./modules/management-app-installers-zoom"
 }
 
 ## Begin Jamf Security Cloud Configuration
 
 ## Create UEMC and Okta integrations
-
-module "configuration-jamf-security-cloud-jamf-pro" {
+module "jsc_uemc" {
+  count                = var.include_jsc_uemc == true ? 1 : 0
   source               = "./modules/configuration-jamf-security-cloud-jamf-pro"
   tje_okta_clientid    = var.tje_okta_clientid
   tje_okta_orgdomain   = var.tje_okta_orgdomain
@@ -227,162 +254,194 @@ module "configuration-jamf-security-cloud-jamf-pro" {
 }
 
 ## Create Jamf Security Cloud Activation Profile containing ALL JSC Services
-
-module "configuration-jamf-security-cloud-all-services" {
+module "jsc_all_services" {
+  count              = var.include_jsc_all_services == true ? 1 : 0
   source             = "./modules/configuration-jamf-security-cloud-all-services"
   tje_okta_clientid  = var.tje_okta_clientid
   tje_okta_orgdomain = var.tje_okta_orgdomain
 }
 
-module "network-security-access-policy-adobe" {
+module "jsc_ap_adobe" {
+  count  = var.include_jsc_ap_adobe == true ? 1 : 0
   source = "./modules/network-security-access-policy-adobe"
 }
 
-module "network-security-access-policy-atlassian" {
+module "jsc_ap_atlassian" {
+  count  = var.include_jsc_ap_atlassian == true ? 1 : 0
   source = "./modules/network-security-access-policy-atlassian"
 }
 
-module "network-security-access-policy-bluejeans" {
+module "jsc_ap_bluejeans" {
+  count  = var.include_jsc_ap_bluejeans == true ? 1 : 0
   source = "./modules/network-security-access-policy-bluejeans"
 }
 
-module "network-security-access-policy-box" {
+module "jsc_ap_box" {
+  count  = var.include_jsc_ap_box == true ? 1 : 0
   source = "./modules/network-security-access-policy-box"
 }
 
-module "network-security-access-policy-docusign" {
+module "jsc_ap_docusign" {
+  count  = var.include_jsc_ap_docusign == true ? 1 : 0
   source = "./modules/network-security-access-policy-docusign"
 }
 
-module "network-security-access-policy-dropbox" {
+module "jsc_ap_dropbox" {
+  count  = var.include_jsc_ap_dropbox == true ? 1 : 0
   source = "./modules/network-security-access-policy-dropbox"
 }
 
-module "network-security-access-policy-github" {
+module "jsc_ap_github" {
+  count  = var.include_jsc_ap_github == true ? 1 : 0
   source = "./modules/network-security-access-policy-github"
 }
 
-module "network-security-access-policy-google" {
+module "jsc_ap_google" {
+  count  = var.include_jsc_ap_google == true ? 1 : 0
   source = "./modules/network-security-access-policy-google"
 }
 
-module "network-security-access-policy-hubspot" {
+module "jsc_ap_hubspot" {
+  count  = var.include_jsc_ap_hubspot == true ? 1 : 0
   source = "./modules/network-security-access-policy-hubspot"
 }
 
-module "network-security-access-policy-mailchimp" {
+module "jsc_ap_mailchimp" {
+  count  = var.include_jsc_ap_mailchimp == true ? 1 : 0
   source = "./modules/network-security-access-policy-mailchimp"
 }
 
-module "network-security-access-policy-mathworks" {
+module "jsc_ap_mathworks" {
+  count  = var.include_jsc_ap_mathworks == true ? 1 : 0
   source = "./modules/network-security-access-policy-mathworks"
 }
 
-module "network-security-access-policy-microsoft" {
+module "jsc_ap_microsoft" {
+  count  = var.include_jsc_ap_microsoft == true ? 1 : 0
   source = "./modules/network-security-access-policy-microsoft"
 }
 
-module "network-security-access-policy-my-ip" {
+module "jsc_ap_my_ip" {
+  count  = var.include_jsc_ap_my_ip == true ? 1 : 0
   source = "./modules/network-security-access-policy-my-ip"
 }
 
-module "network-security-access-policy-okta" {
+module "jsc_ap_okta" {
+  count  = var.include_jsc_ap_okta == true ? 1 : 0
   source = "./modules/network-security-access-policy-okta"
 }
 
-module "network-security-access-policy-salesforce" {
+module "jsc_ap_salesforce" {
+  count  = var.include_jsc_ap_salesforce == true ? 1 : 0
   source = "./modules/network-security-access-policy-salesforce"
 }
 
-module "network-security-access-policy-servicenow" {
+module "jsc_ap_servicenow" {
+  count  = var.include_jsc_ap_servicenow == true ? 1 : 0
   source = "./modules/network-security-access-policy-servicenow"
 }
 
-module "network-security-access-policy-slack" {
+module "jsc_ap_slack" {
+  count  = var.include_jsc_ap_slack == true ? 1 : 0
   source = "./modules/network-security-access-policy-slack"
 }
 
-module "network-security-access-policy-snowflake" {
+module "jsc_ap_snowflake" {
+  count  = var.include_jsc_ap_snowflake == true ? 1 : 0
   source = "./modules/network-security-access-policy-snowflake"
 }
 
-module "network-security-access-policy-splunk" {
+module "jsc_ap_splunk" {
+  count  = var.include_jsc_ap_splunk == true ? 1 : 0
   source = "./modules/network-security-access-policy-splunk"
 }
 
-module "network-security-access-policy-square" {
+module "jsc_ap_square" {
+  count  = var.include_jsc_ap_square == true ? 1 : 0
   source = "./modules/network-security-access-policy-square"
 }
 
-module "network-security-access-policy-twilio" {
+module "jsc_ap_twilio" {
+  count  = var.include_jsc_ap_twilio == true ? 1 : 0
   source = "./modules/network-security-access-policy-twilio"
 }
 
-module "network-security-access-policy-webex" {
+module "jsc_ap_webex" {
+  count  = var.include_jsc_ap_webex == true ? 1 : 0
   source = "./modules/network-security-access-policy-webex"
 }
 
-module "network-security-access-policy-workday" {
+module "jsc_ap_workday" {
+  count  = var.include_jsc_ap_workday == true ? 1 : 0
   source = "./modules/network-security-access-policy-workday"
 }
 
-module "network-security-access-policy-zendesk" {
+module "jsc_ap_zendesk" {
+  count  = var.include_jsc_ap_zendesk == true ? 1 : 0
   source = "./modules/network-security-access-policy-zendesk"
 }
 
-module "network-security-access-policy-zoom" {
+module "jsc_ap_zoom" {
+  count  = var.include_jsc_ap_zoom == true ? 1 : 0
   source = "./modules/network-security-access-policy-zoom"
 }
 
-module "configuration-jamf-security-cloud-block-pages" {
+module "jsc_block_pages" {
+  count           = var.include_jsc_block_pages == true ? 1 : 0
   source          = "./modules/configuration-jamf-security-cloud-block-pages"
   block_page_logo = var.block_page_logo
 }
 
 ## Create Jamf Security Cloud Activation Profile containing ONLY Category Based Content Filtering
-
-module "network-security-jamf-pro-content-filtering" {
+module "jsc_dp_only" {
+  count              = var.include_jsc_dp_only == true ? 1 : 0
   source             = "./modules/network-security-jamf-pro-content-filtering"
   tje_okta_clientid  = var.tje_okta_clientid
   tje_okta_orgdomain = var.tje_okta_orgdomain
 }
 
 ## Create Jamf Security Cloud Activation Profile containing ONLY Threat Response (MTD) 
-
-module "network-security-jamf-pro-network-threat-defense" {
+module "jsc_mtd_only" {
+  count              = var.include_jsc_mtd_only == true ? 1 : 0
   source             = "./modules/network-security-jamf-pro-network-threat-defense"
   tje_okta_clientid  = var.tje_okta_clientid
   tje_okta_orgdomain = var.tje_okta_orgdomain
 }
 
 ## Create Jamf Security Cloud Activation Profile containing ONLY Threat Response (MTD) 
-
-module "network-security-jamf-pro-content-filtering-and-network-threat-defense" {
+module "jsc_mtd_dp_only" {
+  count              = var.include_jsc_mtd_dp_only == true ? 1 : 0
   source             = "./modules/network-security-jamf-pro-content-filtering-and-network-threat-defense"
   tje_okta_clientid  = var.tje_okta_clientid
   tje_okta_orgdomain = var.tje_okta_orgdomain
 }
 
 ## Create Jamf Security Cloud Activation Profile containing ONLY Connect ZTNA
-
-module "network-security-jamf-pro-zero-trust-network-access" {
+module "jsc_ztna" {
+  count              = var.include_jsc_ztna == true ? 1 : 0
   source             = "./modules/network-security-jamf-pro-zero-trust-network-access"
   tje_okta_clientid  = var.tje_okta_clientid
   tje_okta_orgdomain = var.tje_okta_orgdomain
 }
 
 ## Create Jamf Security Cloud Activation Profile containing ONLY Connect ZTNA
-
-module "network-security-jamf-pro-zero-trust-network-access-and-content-filtering" {
+module "jsc_ztna_dp_only" {
+  count              = var.include_jsc_ztna_dp_only == true ? 1 : 0
   source             = "./modules/network-security-jamf-pro-zero-trust-network-access-and-content-filtering"
   tje_okta_clientid  = var.tje_okta_clientid
   tje_okta_orgdomain = var.tje_okta_orgdomain
 }
 
 ## Create Jamf Security Cloud Activation Profile containing ONLY Connect ZTNA
-
-module "network-security-jamf-pro-zero-trust-network-access-and-network-threat-prevention" {
+module "jsc_ztna_mtd_only" {
+  count              = var.include_jsc_ztna_mtd_only == true ? 1 : 0
   source             = "./modules/network-security-jamf-pro-zero-trust-network-access-and-network-threat-prevention"
   tje_okta_clientid  = var.tje_okta_clientid
   tje_okta_orgdomain = var.tje_okta_orgdomain
 }
+
+## Create Jamf Security Cloud Activation Profile containing ONLY Connect Network Relay
+# module "jsc_network_relay" {
+#   count  = var.include_jsc_network_relay == true ? 1 : 0
+#   source = "./modules/network_security_jamf_pro_network_relay"
+# }
