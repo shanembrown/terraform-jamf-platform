7261f4fb1400d9f02691e4ce1a5fd51c19879c3c
leggatron <ryan.legg@jamf.com>
2025-05-19 14:57:55 -0400

    protect changes

--- modules/configuration-jamf-pro-jamf-protect/main.tf
+++ modules/configuration-jamf-pro-jamf-protect/main.tf
@@ -19,3 +19,39 @@ resource "jamfpro_jamf_protect" "settings" {
   }
 
 }
+
+## Create Category
+resource "jamfpro_category" "category_jamfprotect_security" {
+  name = "Security - Jamf Protect"
+}
+
+# Create Smart Group and Congfiguration Profile to identify Sequoia Macs and make Jamf Protect a non removable system extension
+
+resource "jamfpro_smart_computer_group" "group_sequoia_computers_jamf_protect" {
+  name = "Macs on MacOS Sequoia (Jamf Protect System Extension Enforcement) [${random_integer.entropy.result}]"
+  criteria {
+    name        = "Operating System Version"
+    search_type = "like"
+    value       = "15."
+    and_or      = "and"
+    priority    = 0
+  }
+}
+
+resource "jamfpro_macos_configuration_profile_plist" "jamfpro_macos_configuration_profile_jamf_protect_system_extension" {
+  name                = "Jamf Protect System Extension Enforcement [${random_integer.entropy.result}]"
+  description         = "This configuration profile prevents users from disabling the Jamf Protect System Extension"
+  level               = "System"
+  redeploy_on_update  = "Newly Assigned"
+  distribution_method = "Install Automatically"
+  payloads            = file("${path.module}/support_files/non_removable_system_extension_jamf_protect.mobileconfig")
+  payload_validate    = false
+  user_removable      = false
+  category_id         = jamfpro_category.category_jamfprotect_security.id
+
+  scope {
+    all_computers      = false
+    all_jss_users      = false
+    computer_group_ids = [jamfpro_smart_computer_group.group_sequoia_computers_jamf_protect.id]
+  }
+}
