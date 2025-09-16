dd39664b1051c12cddd878c5afe987625a95473a
leggatron <ryan.legg@jamf.com>
2025-09-16 13:27:06 -0400

    added available compliance benchmarks for all 26 OS versions

--- modules/compliance-macOS-cmmc-level-1/main.tf
+++ modules/compliance-macOS-cmmc-level-1/main.tf
@@ -19,6 +19,11 @@ resource "jamfpro_category" "category_sequoia_cmmc_lvl1_benchmarks" {
   priority = 9
 }
 
+resource "jamfpro_category" "category_tahoe_cmmc_lvl1_benchmarks" {
+  name     = "Tahoe - US CMMC 2.0 Level 1 Benchmarks"
+  priority = 9
+}
+
 ## Create scripts
 resource "jamfpro_script" "script_sonoma_cmmc_lvl1_compliance" {
   name            = "Sonoma - US CMMC 2.0 Level 1 Compliance"
@@ -34,6 +39,14 @@ resource "jamfpro_script" "script_sequoia_cmmc_lvl1_compliance" {
   script_contents = file("${path.module}/support_files/computer_scripts/sequoia_cmmc_lvl1_compliance.sh")
   category_id     = jamfpro_category.category_sequoia_cmmc_lvl1_benchmarks.id
   info            = "This script will apply a set of rules related to the US CMMC 2.0 Level 1 benchmark for macOS Sequoia"
+}
+
+resource "jamfpro_script" "script_tahoe_cmmc_lvl1_compliance" {
+  name            = "Tahoe - US CMMC 2.0 Level 1 Compliance"
+  priority        = "AFTER"
+  script_contents = file("${path.module}/support_files/computer_scripts/tahoe_cmmc_lvl1_compliance.sh")
+  category_id     = jamfpro_category.category_tahoe_cmmc_lvl1_benchmarks.id
+  info            = "This script will apply a set of rules related to the US CMMC 2.0 Level 1 benchmark for macOS Tahoe"
 }
 
 ## Create computer extension attributes
@@ -125,6 +138,42 @@ resource "jamfpro_smart_computer_group" "group_sequoia_cmmc_lvl1_non_compliant" 
     name        = "Operating System Version"
     search_type = "like"
     value       = "15."
+    and_or      = "and"
+    priority    = 0
+  }
+  criteria {
+    name        = jamfpro_computer_extension_attribute.ea_cmmc_lvl1_failed_count.name
+    search_type = "more than"
+    value       = "0"
+    and_or      = "and"
+    priority    = 1
+  }
+}
+
+resource "jamfpro_smart_computer_group" "group_tahoe_computers" {
+  name = "US CMMC 2.0 Level 1 - Tahoe Computers"
+  criteria {
+    name        = "Operating System Version"
+    search_type = "like"
+    value       = "26."
+    and_or      = "and"
+    priority    = 0
+  }
+  criteria {
+    name        = "Serial Number"
+    search_type = "like"
+    value       = "111222333444555"
+    and_or      = "and"
+    priority    = 1
+  }
+}
+
+resource "jamfpro_smart_computer_group" "group_tahoe_cmmc_lvl1_non_compliant" {
+  name = "US CMMC 2.0 Level 1 - Tahoe - Non Compliant Computers"
+  criteria {
+    name        = "Operating System Version"
+    search_type = "like"
+    value       = "26."
     and_or      = "and"
     priority    = 0
   }
@@ -294,6 +343,84 @@ resource "jamfpro_policy" "policy_sequoia_cmmc_lvl1_remediation" {
   }
 }
 
+resource "jamfpro_policy" "policy_tahoe_cmmc_lvl1_audit" {
+  name            = "US CMMC 2.0 Level 1 - Audit (Tahoe)"
+  enabled         = true
+  trigger_checkin = true
+  frequency       = "Ongoing"
+  category_id     = jamfpro_category.category_tahoe_cmmc_lvl1_benchmarks.id
+
+  scope {
+    all_computers      = false
+    computer_group_ids = [jamfpro_smart_computer_group.group_tahoe_computers.id]
+  }
+
+  self_service {
+    use_for_self_service = false
+  }
+
+  payloads {
+    scripts {
+      id         = jamfpro_script.script_tahoe_cmmc_lvl1_compliance.id
+      parameter4 = "--check"
+    }
+
+    maintenance {
+      recon = true
+    }
+
+    reboot {
+      file_vault_2_reboot            = false
+      message                        = "This computer will restart in 5 minutes. Please save anything you are working on and log out by choosing Log Out from the bottom of the Apple menu."
+      minutes_until_reboot           = 5
+      no_user_logged_in              = "Do not restart"
+      start_reboot_timer_immediately = false
+      startup_disk                   = "Current Startup Disk"
+      user_logged_in                 = "Do not restart"
+    }
+  }
+}
+
+resource "jamfpro_policy" "policy_tahoe_cmmc_lvl1_remediation" {
+  name            = "US CMMC 2.0 Level 1 - Remediation (Tahoe)"
+  enabled         = true
+  trigger_checkin = true
+  frequency       = "Ongoing"
+  category_id     = jamfpro_category.category_tahoe_cmmc_lvl1_benchmarks.id
+
+  scope {
+    all_computers      = false
+    computer_group_ids = [jamfpro_smart_computer_group.group_tahoe_cmmc_lvl1_non_compliant.id]
+  }
+
+  self_service {
+    use_for_self_service = false
+  }
+
+  payloads {
+    scripts {
+      id         = jamfpro_script.script_tahoe_cmmc_lvl1_compliance.id
+      parameter4 = "--check"
+      parameter5 = "--fix"
+      parameter6 = "--check"
+    }
+
+    maintenance {
+      recon = true
+    }
+
+    reboot {
+      file_vault_2_reboot            = false
+      message                        = "This computer will restart in 5 minutes. Please save anything you are working on and log out by choosing Log Out from the bottom of the Apple menu."
+      minutes_until_reboot           = 5
+      no_user_logged_in              = "Do not restart"
+      start_reboot_timer_immediately = false
+      startup_disk                   = "Current Startup Disk"
+      user_logged_in                 = "Do not restart"
+    }
+  }
+}
+
 ## Define configuration profile details for Sonoma
 locals {
   sonoma_cmmc_lvl1_dict = {
@@ -395,6 +522,61 @@ resource "jamfpro_macos_configuration_profile_plist" "sequoia_cmmc_lvl1_smart_ca
   level               = "System"
 
   payloads         = file("${path.module}/support_files/computer_config_profiles/Sequoia_cmmc_lvl1-security.smartcard.mobileconfig")
+  payload_validate = false
+
+  scope {
+    all_computers      = false
+    computer_group_ids = []
+  }
+}
+
+## Define configuration profile details for Tahoe
+locals {
+  tahoe_cmmc_lvl1_dict = {
+    "Accessibility"          = "${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-Accessibility.mobileconfig"
+    "Application Access"     = "${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-applicationaccess.mobileconfig"
+    "Assistant"              = "${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-assistant.support.mobileconfig"
+    "iCloud"                 = "${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-icloud.managed.mobileconfig"
+    "Login Window"           = "${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-loginwindow.mobileconfig"
+    "MCX"                    = "${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-MCX.mobileconfig"
+    "Photos Shared Defauts"  = "${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-photos.shareddefaults.mobileconfig"
+    "Firewall"               = "${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-security.firewall.mobileconfig"
+    "Setup Assistant"        = "${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-SetupAssistant.managed.mobileconfig"
+    "Software Update"        = "${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-SoftwareUpdate.mobileconfig"
+    "Submit Diagnostic Info" = "${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-SubmitDiagInfo.mobileconfig"
+    "System Policy Control"  = "${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-systempolicy.control.mobileconfig"
+    "System Preferences"     = "${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-systempreferences.mobileconfig"
+  }
+}
+
+## Create configuration profiles for Tahoe
+resource "jamfpro_macos_configuration_profile_plist" "tahoe_cmmc_lvl1" {
+  for_each            = local.tahoe_cmmc_lvl1_dict
+  name                = "Tahoe US CMMC 2.0 Level 1 - ${each.key}"
+  description         = "To scope this configuration profile, navigate to Smart Computer Groups, select the 'US CMMC 2.0 Level 1 - Tahoe Computers' Smart Group and remove the placeholder serial number criteria."
+  distribution_method = "Install Automatically"
+  redeploy_on_update  = "Newly Assigned"
+  category_id         = jamfpro_category.category_tahoe_cmmc_lvl1_benchmarks.id
+  level               = "System"
+
+  payloads         = file("${each.value}")
+  payload_validate = false
+
+  scope {
+    all_computers      = false
+    computer_group_ids = [jamfpro_smart_computer_group.group_tahoe_computers.id]
+  }
+  depends_on = [jamfpro_macos_configuration_profile_plist.sequoia_cmmc_lvl1]
+}
+
+resource "jamfpro_macos_configuration_profile_plist" "tahoe_cmmc_lvl1_smart_card" {
+  name                = "Tahoe US CMMC 2.0 Level 1 - Smart Card"
+  distribution_method = "Install Automatically"
+  redeploy_on_update  = "Newly Assigned"
+  category_id         = jamfpro_category.category_tahoe_cmmc_lvl1_benchmarks.id
+  level               = "System"
+
+  payloads         = file("${path.module}/support_files/computer_config_profiles/Tahoe_cmmc_lvl1-security.smartcard.mobileconfig")
   payload_validate = false
 
   scope {
--- modules/compliance-macOS-nist-800-171/main.tf
+++ modules/compliance-macOS-nist-800-171/main.tf
@@ -19,6 +19,11 @@ resource "jamfpro_category" "category_sequoia_800_171_benchmarks" {
   priority = 9
 }
 
+resource "jamfpro_category" "category_tahoe_800_171_benchmarks" {
+  name     = "Tahoe - NIST 800-171 Benchmarks"
+  priority = 9
+}
+
 ## Create scripts
 resource "jamfpro_script" "script_sonoma_800_171_compliance" {
   name            = "Sonoma - NIST 800-171 Compliance"
@@ -34,6 +39,14 @@ resource "jamfpro_script" "script_sequoia_800_171_compliance" {
   script_contents = file("${path.module}/support_files/computer_scripts/sequoia_800-171_compliance.sh")
   category_id     = jamfpro_category.category_sequoia_800_171_benchmarks.id
   info            = "This script will apply a set of rules related to the NIST 800-171 benchmark for macOS Sequoia"
+}
+
+resource "jamfpro_script" "script_tahoe_800_171_compliance" {
+  name            = "Tahoe - NIST 800-171 Compliance"
+  priority        = "AFTER"
+  script_contents = file("${path.module}/support_files/computer_scripts/tahoe_800-171_compliance.sh")
+  category_id     = jamfpro_category.category_sequoia_800_171_benchmarks.id
+  info            = "This script will apply a set of rules related to the NIST 800-171 benchmark for macOS Tahoe"
 }
 
 ## Create computer extension attributes
@@ -125,6 +138,42 @@ resource "jamfpro_smart_computer_group" "group_sequoia_800_171_non_compliant" {
     name        = "Operating System Version"
     search_type = "like"
     value       = "15."
+    and_or      = "and"
+    priority    = 0
+  }
+  criteria {
+    name        = jamfpro_computer_extension_attribute.ea_800_171_failed_count.name
+    search_type = "more than"
+    value       = "0"
+    and_or      = "and"
+    priority    = 1
+  }
+}
+
+resource "jamfpro_smart_computer_group" "group_tahoe_computers" {
+  name = "NIST 800-171 - Tahoe Computers"
+  criteria {
+    name        = "Operating System Version"
+    search_type = "like"
+    value       = "26."
+    and_or      = "and"
+    priority    = 0
+  }
+  criteria {
+    name        = "Serial Number"
+    search_type = "like"
+    value       = "111222333444555"
+    and_or      = "and"
+    priority    = 1
+  }
+}
+
+resource "jamfpro_smart_computer_group" "group_tahoe_800_171_non_compliant" {
+  name = "NIST 800-171 - Tahoe - Non Compliant Computers"
+  criteria {
+    name        = "Operating System Version"
+    search_type = "like"
+    value       = "26."
     and_or      = "and"
     priority    = 0
   }
@@ -294,6 +343,84 @@ resource "jamfpro_policy" "policy_sequoia_800_171_remediation" {
   }
 }
 
+resource "jamfpro_policy" "policy_tahoe_800_171_audit" {
+  name            = "NIST 800-171 - Audit (Tahoe)"
+  enabled         = true
+  trigger_checkin = true
+  frequency       = "Ongoing"
+  category_id     = jamfpro_category.category_tahoe_800_171_benchmarks.id
+
+  scope {
+    all_computers      = false
+    computer_group_ids = [jamfpro_smart_computer_group.group_tahoe_computers.id]
+  }
+
+  self_service {
+    use_for_self_service = false
+  }
+
+  payloads {
+    scripts {
+      id         = jamfpro_script.script_tahoe_800_171_compliance.id
+      parameter4 = "--check"
+    }
+
+    maintenance {
+      recon = true
+    }
+
+    reboot {
+      file_vault_2_reboot            = false
+      message                        = "This computer will restart in 5 minutes. Please save anything you are working on and log out by choosing Log Out from the bottom of the Apple menu."
+      minutes_until_reboot           = 5
+      no_user_logged_in              = "Do not restart"
+      start_reboot_timer_immediately = false
+      startup_disk                   = "Current Startup Disk"
+      user_logged_in                 = "Do not restart"
+    }
+  }
+}
+
+resource "jamfpro_policy" "policy_tahoe_800_171_remediation" {
+  name            = "NIST 800-171 - Remediation (Tahoe)"
+  enabled         = true
+  trigger_checkin = true
+  frequency       = "Ongoing"
+  category_id     = jamfpro_category.category_tahoe_800_171_benchmarks.id
+
+  scope {
+    all_computers      = false
+    computer_group_ids = [jamfpro_smart_computer_group.group_tahoe_800_171_non_compliant.id]
+  }
+
+  self_service {
+    use_for_self_service = false
+  }
+
+  payloads {
+    scripts {
+      id         = jamfpro_script.script_tahoe_800_171_compliance.id
+      parameter4 = "--check"
+      parameter5 = "--fix"
+      parameter6 = "--check"
+    }
+
+    maintenance {
+      recon = true
+    }
+
+    reboot {
+      file_vault_2_reboot            = false
+      message                        = "This computer will restart in 5 minutes. Please save anything you are working on and log out by choosing Log Out from the bottom of the Apple menu."
+      minutes_until_reboot           = 5
+      no_user_logged_in              = "Do not restart"
+      start_reboot_timer_immediately = false
+      startup_disk                   = "Current Startup Disk"
+      user_logged_in                 = "Do not restart"
+    }
+  }
+}
+
 ## Define configuration profile details for Sonoma
 locals {
   sonoma_800_171_dict = {
@@ -417,6 +544,72 @@ resource "jamfpro_macos_configuration_profile_plist" "sequoia_800_171_smart_card
   level               = "System"
 
   payloads         = file("${path.module}/support_files/computer_config_profiles/Sequoia_800-171-security.smartcard.mobileconfig")
+  payload_validate = false
+
+  scope {
+    all_computers      = false
+    computer_group_ids = []
+  }
+}
+
+## Define configuration profile details for Tahoe
+locals {
+  tahoe_800_171_dict = {
+    "Accessibility"         = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-Accessibility.mobileconfig"
+    "Application Access"    = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-applicationaccess.mobileconfig"
+    "Assistant Support"     = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-assistant.support.mobileconfig"
+    "Disc Recording"        = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-DiscRecording.mobileconfig"
+    "Dock"                  = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-dock.mobileconfig"
+    "Apple IR Controller"   = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-driver.AppleIRController.mobileconfig"
+    "Finder"                = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-finder.mobileconfig"
+    "Global Preferences"    = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-GlobalPreferences.mobileconfig"
+    "iCloud"                = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-icloud.managed.mobileconfig"
+    "Login Window"          = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-loginwindow.mobileconfig"
+    "MCX"                   = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-MCX.mobileconfig"
+    "MCX Bluetooth"         = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-MCXBluetooth.mobileconfig"
+    "mDNS Responder"        = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-mDNSResponder.mobileconfig"
+    "Password Policy"       = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-mobiledevice.passwordpolicy.mobileconfig"
+    "Photos Shared Defauts" = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-photos.shareddefaults.mobileconfig"
+    "Screen Saver"          = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-screensaver.mobileconfig"
+    "Firewall"              = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-security.firewall.mobileconfig"
+    "Setup Assistant"       = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-SetupAssistant.managed.mobileconfig"
+    "Submit Diag Info"      = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-SubmitDiagInfo.mobileconfig"
+    "System Policy Control" = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-systempolicy.control.mobileconfig"
+    "Managed System Policy" = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-systempolicy.managed.mobileconfig"
+    "System Preferences"    = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-systempreferences.mobileconfig"
+    "Managed Client"        = "${path.module}/support_files/computer_config_profiles/Tahoe_800-171-timed.mobileconfig"
+  }
+}
+
+## Create configuration profiles for Tahoe
+resource "jamfpro_macos_configuration_profile_plist" "tahoe_800_171" {
+  for_each            = local.tahoe_800_171_dict
+  name                = "Tahoe NIST 800-171 - ${each.key}"
+  description         = "To scope this configuration profile, navigate to Smart Computer Groups, select the 'NIST 800-171 - Tahoe Computers' Smart Group and remove the placeholder serial number criteria."
+  distribution_method = "Install Automatically"
+  redeploy_on_update  = "Newly Assigned"
+  category_id         = jamfpro_category.category_tahoe_800_171_benchmarks.id
+  level               = "System"
+
+  payloads         = file("${each.value}")
+  payload_validate = false
+
+  scope {
+    all_computers      = false
+    computer_group_ids = [jamfpro_smart_computer_group.group_tahoe_computers.id]
+  }
+  depends_on = [jamfpro_macos_configuration_profile_plist.sequoia_800_171]
+}
+
+resource "jamfpro_macos_configuration_profile_plist" "tahoe_800_171_smart_card" {
+  name                = "Tahoe NIST 800-171 - Smart Card"
+  description         = "To scope this configuration profile, navigate to the Scope tab above and add the 'NIST 800-171 - Tahoe Computers' smart group. Then, be sure to navigate to Smart Computer Groups, select that group and remove the placeholder serial number. This configuration profile is not scoped intentionally due to potential issues that Smart Cards may cause on an endpoint."
+  distribution_method = "Install Automatically"
+  redeploy_on_update  = "Newly Assigned"
+  category_id         = jamfpro_category.category_tahoe_800_171_benchmarks.id
+  level               = "System"
+
+  payloads         = file("${path.module}/support_files/computer_config_profiles/Tahoe_800-171-security.smartcard.mobileconfig")
   payload_validate = false
 
   scope {
